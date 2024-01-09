c2065627bca528cf25b661be08810690c5a7f200
Larissa Bravo <lahbravo@hotmail.com>
2024-01-09 20:51:06 -0300

    Testes produto atualizado

--- cypress/e2e/UI/produtos.cy.js
+++ cypress/e2e/UI/produtos.cy.js
@@ -1,38 +1,26 @@
+/// <reference types="cypress" />
+
 describe('Funcionalidade Página de Produtos', () => {
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+        cy.visit('produtos/')
+    })
+
+    afterEach(() => {
+        cy.get('.woocommerce-message').should('contain', 'adicionado no seu carrinho.')
+    })
+
+    it('Deve adicionar produtos ao carrinho', () => {
+        cy.addProdutos('Argus All-Weather Tank', 'M', 'Gray')
     });
 
-    it.only('Devo selecionar um produto da lista', () => {
-        //        cy.get('.product-block').first().click() -- utilizando apenas uma class
-
-        // utilizando mais de uma classe
-        cy.get('[class="product-block grid"]')
-            // .first() --primeiro da lista
-            // .last() -- o último da lista
-            // .eq(4) -- pega determinado item da lista
-            .contains('Argus All-Weather Tank')
-            .click()
-
-    });
-
-    it('Devo adicionar um produto ao carrinho', () => {
-        var quantidade = 2
-
-        cy.get('[class="product-block grid"]').contains('Argus All-Weather Tank').click()
-        cy.get('.button-variable-item-M').click()
-        cy.get('.button-variable-item-Gray').click()
-        cy.get('.input-text').clear().type(quantidade)
-        cy.get('.single_add_to_cart_button').click()
-
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
     
+    it('Deve adicionar um novo produto ao carrinho', () => {
+        cy.addProdutos('Aero Daily Fitness Tee', 'XS', 'Black')
     });
 
-    it('Deve adicionar produtos ao carrinho - Usando Comando Customizado', () => {
-        cy.addProdutos('Argus All-Weather Tank', 'M', 'Gray', 3)
-
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 3)
+    
+    it('Deve adicionar um novo produto ao carrinho', () => {
+        cy.addProdutos('Aether Gym Pant', '33', 'Blue')
     });
 
 });
